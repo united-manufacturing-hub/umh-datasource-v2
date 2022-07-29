e6de5e269b1cd5851741a7c41488f0168e0585d7
JeremyTheocharis <jt@umh.app>
2022-07-29 13:18:30 +0200

    chore: formatted more code

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,9 @@
-import { DataSourcePlugin } from '@grafana/data';
-import { DataSource } from './datasource';
-import { ConfigEditor } from './ConfigEditor';
-import { QueryEditor } from './QueryEditor';
+import {DataSourcePlugin} from '@grafana/data';
+import {DataSource} from './datasource';
+import {ConfigEditor} from './ConfigEditor';
+import {QueryEditor} from './QueryEditor';
 import {FactoryinsightDataSourceOptions, FactoryinsightQuery} from './types';
 
 export const plugin = new DataSourcePlugin<DataSource, FactoryinsightQuery, FactoryinsightDataSourceOptions>(DataSource)
-  .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditor);
+    .setConfigEditor(ConfigEditor)
+    .setQueryEditor(QueryEditor);
--- src/types.ts
+++ src/types.ts
@@ -1,22 +1,22 @@
-import { DataQuery, DataSourceJsonData } from '@grafana/data';
+import {DataQuery, DataSourceJsonData} from '@grafana/data';
 
 export interface FactoryinsightQuery extends DataQuery {
-  // to be removed
-  location?: string;
-  asset?: string;
+    // to be removed
+    location?: string;
+    asset?: string;
 
-  enterprise?: string;
-  site?: string;
-  area?: string;
-  productionLine?: string;
-  workCell?: string;
-  value?: string;
+    enterprise?: string;
+    site?: string;
+    area?: string;
+    productionLine?: string;
+    workCell?: string;
+    value?: string;
 }
 
 export const defaultFactoryinsightQuery: Partial<FactoryinsightQuery> = {
-  // to be removed
-  location: '', // TODO TESTDATA
-  asset: '',
+    // to be removed
+    location: '', // TODO TESTDATA
+    asset: '',
 
     enterprise: '',
     site: '',
@@ -30,13 +30,13 @@ export const defaultFactoryinsightQuery: Partial<FactoryinsightQuery> = {
  * These are options configured for each DataSource instance
  */
 export interface FactoryinsightDataSourceOptions extends DataSourceJsonData {
-  baseURL?: string;
-  customerID?: string;
+    baseURL?: string;
+    customerID?: string;
 }
 
 /**
  * Value that is used in the backend, but never sent over HTTP to the frontend
  */
 export interface FactoryinsightSecureJsonData {
-  apiKey?: string;
+    apiKey?: string;
 }
